ccc894f1f8da5212656304a38ea18e9773d26562
Павел <https://github.com/PavelZabaronak>
2021-06-20 22:02:44 +0300

    gggg

--- Doctor/src/com/hospital/doctor/controller/ControlerLogin.java
+++ Doctor/src/com/hospital/doctor/controller/ControlerLogin.java
@@ -1,8 +1,14 @@
 package com.hospital.doctor.controller;
 
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class ControlerLogin {
 
@@ -13,8 +19,33 @@ public class ControlerLogin {
     private URL location;
 
     @FXML
+    private TextField USR;
+
+    @FXML
+    private PasswordField RRRR;
+
+    @FXML
+    private Button STSRT;
+
+    @FXML
+    private Button TTTTT;
+
+    @FXML
+    void STSRT(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tttttt(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert USR != null : "fx:id=\"USR\" was not injected: check your FXML file 'Login.fxml'.";
+        assert RRRR != null : "fx:id=\"RRRR\" was not injected: check your FXML file 'Login.fxml'.";
+        assert STSRT != null : "fx:id=\"STSRT\" was not injected: check your FXML file 'Login.fxml'.";
+        assert TTTTT != null : "fx:id=\"TTTTT\" was not injected: check your FXML file 'Login.fxml'.";
 
     }
 }
-
